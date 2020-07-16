37c1a2e0ed4dd6419a883c0768c74d0aacf20d85
Stephen McVeigh <smcveigh941@gmail.com>
2020-07-16 20:47:53 +0100

    Redesigned website

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
-import os
 import json
+import os
+import sys
+from builtins import dict, open
 
 CONFIG_PATH = "config.json"
 
@@ -7,17 +9,17 @@ CONFIG_PATH = "config.json"
 def get_project_settings():
     if not os.path.isfile(CONFIG_PATH):
         print('A file with the name "config.json" was not found in the root directory')
-        exit(1)
+        sys.exit(1)
 
-    config = json.loads(open(CONFIG_PATH).read())
+    with open(CONFIG_PATH, 'r') as f:
+        config = json.load(f)
+
     settings = dict(
         MAIL_SERVER=config['MAIL_SERVER'],
         MAIL_PORT=config['MAIL_PORT'],
         MAIL_USERNAME=config['MAIL_USERNAME'],
         MAIL_PASSWORD=config['MAIL_PASSWORD'],
         MAIL_RECIPIENTS=config['MAIL_RECIPIENTS'],
-        AUTH_PASSWORD=config['AUTH_PASSWORD'],
-        DATABASE='data.db',
         SECRET_KEY=os.urandom(24),
         MAIL_USE_TLS=False,
         MAIL_USE_SSL=True,
--- static/js/init.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('.slider').slider({full_width: true, interval: 5000, indicators: false});
-    $(".button-collapse").sideNav();
-    $('.modal').modal();
-});
